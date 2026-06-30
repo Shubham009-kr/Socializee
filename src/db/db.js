c04b2628a5b173b0694f06175a41e305bad826a6
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-06-30 22:18:59 +0530

    added post and fetch functionality for the posts

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://mrshubhamkr2000_db_user:L9KG9df6ghXSJNuk@cluster0.seqrteg.mongodb.net/socializee-wall");
+    await mongoose.connect(process.env.MONGODB_URL);
 
     console.log("MongoDB connected successfully");
 }
