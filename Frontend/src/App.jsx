d3174d7cbcc1216eec33f8ee9a5cc16788db5921
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-07-02 21:39:33 +0530

    added frontend file and folder structure.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/about" element={<div>About Page</div>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
